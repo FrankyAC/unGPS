549be8e9c2736fb2287b0b57dda401d805feee99
Franky <franalfaro1997@gmail.com>
2018-10-03 18:32:25 +0200

    TravisCI ahora crea imagenes con los binarios contruidos y los sube a Docker Hub

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-while ! curl http://mongo:27017/
+While !curl http://mongo:27017/
 do
-  echo "$(date) - still trying"
-  sleep 1
-done
-echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+	echo "$(date) - still trying"
+
+	sleep 1
+
+done
+
+echo "$(date) - it seems mongo server is connected successfully"
+
+java -jar usr/local/app/ordering-service-0.1.0.jar \\
+
+	-spring.config.location=application.properties # o *.jar
